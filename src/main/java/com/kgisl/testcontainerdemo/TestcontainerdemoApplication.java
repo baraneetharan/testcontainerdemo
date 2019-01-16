7b5e7693982f3033817964ad946a30457a1e0167
Trainee GSS <trainee@kgisl.com>
2019-01-16 15:45:54 +0530

    first commit

---
+++ src/main/java/com/kgisl/testcontainerdemo/TestcontainerdemoApplication.java
@@ -0,0 +1,14 @@
+package com.kgisl.testcontainerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainerdemoApplication.class, args);
+	}
+
+}
+
