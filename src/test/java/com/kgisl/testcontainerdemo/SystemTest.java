4495f116b45f71ad97d5cbf953a8993c818c1196
Trainee GSS <trainee@kgisl.com>
2019-01-16 18:43:50 +0530

    wiki

--- src/test/java/com/kgisl/testcontainerdemo/SystemTest.java
+++ src/test/java/com/kgisl/testcontainerdemo/SystemTest.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testcontainers.containers.*;
 import org.testcontainers.containers.wait.strategy.Wait;
+
+import static org.junit.Assert.assertTrue;
+
 // import org.testcontainers.utility.MountableFile;​
 import java.io.File;
 // import java.util.stream.Stream;​
@@ -23,9 +26,24 @@ public class SystemTest {
 
     @Test
     public void seleniumTest() {
-        RemoteWebDriver webDriver = browser.getWebDriver();
-        webDriver.get("https://www.google.com/");
-        WebElement content = webDriver.findElementById("q");
-        assert content.getText().equals("Foobar");
+        RemoteWebDriver driver = browser.getWebDriver();
+        // webDriver.get("https://www.google.com/");
+        // WebElement content = webDriver.findElementById("q");
+        // assert content.getText().equals("Foobar");
+
+        driver.get("https://wikipedia.org");
+        WebElement searchInput = driver.findElementByName("search");
+
+        searchInput.sendKeys("Rick Astley");
+        searchInput.submit();
+
+        WebElement otherPage = driver.findElementByLinkText("Rickrolling");
+        otherPage.click();
+
+        boolean expectedTextFound = driver.findElementsByCssSelector("p")
+                .stream()
+                .anyMatch(element -> element.getText().contains("meme"));
+
+        assertTrue("The word 'meme' is found on a page about rickrolling", expectedTextFound);
     }
 }
