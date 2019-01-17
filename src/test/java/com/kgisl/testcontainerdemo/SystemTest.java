55e539099229d663f1a1f405b0c823dcb14d6222
Trainee GSS <trainee@kgisl.com>
2019-01-17 17:43:01 +0530

    selenium dependency updated

--- src/test/java/com/kgisl/testcontainerdemo/SystemTest.java
+++ src/test/java/com/kgisl/testcontainerdemo/SystemTest.java
@@ -22,17 +22,19 @@ import java.io.File;
 public class SystemTest {
     private static Network net = Network.newNetwork();
     @Rule
-public BrowserWebDriverContainer chrome
-  = new BrowserWebDriverContainer()
-    .withDesiredCapabilities(DesiredCapabilities.chrome());
- 
-@Test
-public void whenNavigatedToPage_thenHeadingIsInThePage() {
-    RemoteWebDriver driver = chrome.getWebDriver();
-    driver.get("https://saucelabs.com/test/guinea-pig");
-    String heading = driver
-      .findElement(By.xpath("/html/body/h1")).getText();
-  
-    assertEquals("This page is a Selenium sandbox", heading);
-}
+    public BrowserWebDriverContainer chrome = new BrowserWebDriverContainer()
+            .withDesiredCapabilities(DesiredCapabilities.chrome());
+
+    @Test
+    public void whenNavigatedToPage_thenHeadingIsInThePage() {
+        RemoteWebDriver driver = chrome.getWebDriver();
+        driver.get("https://saucelabs.com/test/guinea-pig");
+        String heading = driver.findElement(By.xpath("/html/body/h1")).getText();
+        System.out.println("*************************************************");
+        System.out.println("*************************************************");
+        System.out.println("Selenium Test Container test runs");
+        System.out.println("*************************************************");
+        System.out.println("*************************************************");
+        assertEquals("This page is a Selenium sandbox", heading);
+    }
 }
